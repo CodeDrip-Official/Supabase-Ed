fdec86e7368f153a634ef4ffd576b7d03533ed27
CodeDrip-Official <codedripoff@gmail.com>
2023-08-02 12:57:49 +0800

    Add SignIn UI

--- app/src/main/java/com/example/supabaseed/MainActivity.kt
+++ app/src/main/java/com/example/supabaseed/MainActivity.kt
@@ -4,43 +4,38 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.dimensionResource
+import com.example.supabaseed.presentation.auth.AuthScreen
 import com.example.supabaseed.ui.theme.SupabaseEdTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SupabaseEdTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Scaffold() { innerPadding ->
+                    Surface(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        color = MaterialTheme.colorScheme.background
+                    ) {
+                        AuthScreen(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(dimensionResource(id = R.dimen.padding_xl))
+                        )
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SupabaseEdTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/supabaseed/ui/theme/Type.kt
+++ app/src/main/java/com/example/supabaseed/ui/theme/Type.kt
@@ -2,33 +2,43 @@ package com.example.supabaseed.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.supabaseed.R
+
+val LexendDeca = FontFamily(
+    Font(R.font.lexend_deca),
+    Font(R.font.lexend_deca_medium, FontWeight.Medium)
+)
+
+val Cabin = FontFamily(
+    Font(R.font.cabin),
+    Font(R.font.cabin_medium, FontWeight.Medium),
+    Font(R.font.cabin_semibold, FontWeight.SemiBold),
+    Font(R.font.cabin_bold, FontWeight.Bold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
+    headlineLarge = TextStyle(
+        fontFamily = LexendDeca,
+        fontWeight = FontWeight.Medium,
+        fontSize = 36.sp,
+        lineHeight = 44.sp,
         letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+
+    titleLarge = Typography().titleLarge.copy(fontFamily = Cabin),
+    titleMedium = Typography().titleMedium.copy(fontFamily = Cabin),
+    titleSmall = Typography().titleSmall.copy(fontFamily = Cabin),
+
+    labelLarge = Typography().labelLarge.copy(fontFamily = Cabin),
+    labelMedium = Typography().labelMedium.copy(fontFamily = Cabin),
+    labelSmall = Typography().labelSmall.copy(fontFamily = Cabin),
+
+    bodyLarge = Typography().bodyLarge.copy(fontFamily = Cabin),
+    bodyMedium = Typography().bodyMedium.copy(fontFamily = Cabin),
+    bodySmall = Typography().bodySmall.copy(fontFamily = Cabin),
 )
